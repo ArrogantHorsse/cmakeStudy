d7d5c5e505eedd5f02a49d65bcb41134d6ee84f6
mtj_ubuntu <mtj_ubuntu@163.com>
2024-12-08 14:21:28 +0800

    Action : add CMAKE_BUILD_TYPE

--- 01-Basic/A-hello-cmake/main.cpp
+++ 01-Basic/A-hello-cmake/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+class BinaryBuffer {
+public:
+    BinaryBuffer(std::vector<std::string> data)
+        : data_(data) {}
+
+    BinaryBuffer(BinaryBuffer&& other) noexcept
+        : data_(std::move(other.data_)) {
+    }
+
+    
+    void doSomeThing() {
+        std::cout << "Doing something ssss data." << std::endl;
+    }
+
+private:
+    std::vector<std::string> data_;
+};
 
 int main() {
-    std::cout << "HelloWorld!" << std::endl;
+    BinaryBuffer clientObjA(std::vector<std::string>{"0", "0"});
+    BinaryBuffer clientObjB(std::move(clientObjA));
+    clientObjA.doSomeThing(); // 访问
+    // if (clientObjA.isValid()) {
+    //     clientObjA.doSomeThing(); // 访问
+    // } else {
+    //     return 0; // 早退
+    // }
+    
     return 0;
 }
